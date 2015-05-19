f10ca258c1e504ff3ef5cbdbb5729e442efd7a07
Jerimiah Milton <milton_jerimiah@bah.com>
2015-05-19 09:47:00 -0500

    Commented out the pollvms rake task per @mafernando

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,11 +2,11 @@
 
 # Learn more: http://github.com/jmettraux/rufus-scheduler
 
-require 'rufus-scheduler'
-require 'rake'
-
-scheduler = Rufus::Scheduler.new
-
-scheduler.every '15m' do
-  Rake::Task['jellyfish:manageiq:pollvms']
-end
+# require 'rufus-scheduler'
+# require 'rake'
+#
+# scheduler = Rufus::Scheduler.new
+#
+# scheduler.every '15m' do
+#   Rake::Task['jellyfish:manageiq:pollvms']
+# end
